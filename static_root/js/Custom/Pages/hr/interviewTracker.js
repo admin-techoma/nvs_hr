a588206fbdbc0ad260aced57dd585a9a1b16e910
Vishal <vishpatel1595@gmail.com>
2024-08-01 11:31:01 +0530

    ui changes bug fix 2

---
+++ static_root/js/Custom/Pages/hr/interviewTracker.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#interviewTrackerTable').DataTable({
+        responsive: true,
+        searching: true,
+        paging: true,
+        fixedHeader: true
+    });
+});

8cbca2d0f640ca545a066b2352ed175c04f8a0bb
MS-Patel <patelmilind1620@gmail.com>
2024-08-05 13:45:26 +0530

    cahnges

--- core/settings.py
+++ core/settings.py
@@ -103,23 +103,23 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'nvs_hr',         
-        'USER': 'admin',       
-        'PASSWORD': 'P@$$w0rd',    
-        'HOST': 'localhost',             
-        'PORT': '',              
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'nvs_hr',         
+#         'USER': 'admin',       
+#         'PASSWORD': 'P@$$w0rd',    
+#         'HOST': 'localhost',             
+#         'PORT': '',              
+#     }
+# }
 
 
 # Password validation
